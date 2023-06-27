14fd074b28306566e2650a310eafeb6eb2359c0a
Sabrin <cse_2012020251@lus.ac.bd>
2023-06-28 01:08:48 +0600

    commit

--- loginAction.php
+++ loginAction.php
@@ -8,7 +8,7 @@ $input_pass = $_POST['l_pass'];
 if($input_username == $username && $input_pass==$pass)
 {
     $_SESSION['l_username']=$username;
-    echo "<script> location.href = 'product.php' </script>";
+    echo "<script> location.href = 'home.php' </script>";
 }
 else{
     echo "<script> alert ('Username and Password is not matching!!')</script>";

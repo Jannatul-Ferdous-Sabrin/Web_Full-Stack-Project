3fff377de2dbed9974fb39a6f4727b8d239ff958
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-06 00:32:10 +0600

    commit

---
+++ Auth/loginAction.php
@@ -0,0 +1,24 @@
+ 
+ 
+ <!-- <?php
+session_start();
+$username="Sabrin";
+$pass="Sabb";
+$input_username = $_POST['l_username'];
+$input_pass = $_POST['l_pass'];
+
+if($input_username == $username && $input_pass==$pass)
+{
+    $_SESSION['l_username']=$username;
+    echo "<script> location.href = 'home.php' </script>";
+}
+else{
+    echo "<script> alert ('Username and Password is not matching!!')</script>";
+    echo "<script> location.href = 'login.php' </script>";
+}
+?> -->
+
+
+
+
+

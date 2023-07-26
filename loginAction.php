e8c57a9effea687cc29c816cf96a23d9307cb70a
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-27 00:38:50 +0600

    commit

--- loginAction.php
+++ loginAction.php
@@ -1,21 +1,27 @@
  
   <?php
-session_start();
-$username="Sabrin";
-$pass="Sabb";
-$input_username = $_POST['l_username'];
-$input_pass = $_POST['l_pass'];
+if (isset($_POST['login'])) {
+  include 'config.php';
+  $l_username = $_POST['l_username'];
+  $l_pass = $_POST['l_pass'];
 
-if($input_username == $username && $input_pass==$pass)
-{
-    $_SESSION['l_username']=$username;
-    echo "<script> location.href = './CURD/index.php' </script>";
+$result = mysqli_query($conn, "SELECT * FROM `register` WHERE db_username='$l_username' AND db_password='$l_pass'");
+
+ if (!$result) {
+        echo "Query execution failed: " . mysqli_error($conn);
+    } else {
+        if (mysqli_num_rows($result) > 0) {
+            session_start();
+            $_SESSION['username'] = $l_username; 
+            echo "<script>location.href='./curd/index.php'</script>";
+        } else {
+            echo "<script>alert('Invalid username and password!')</script>";
+            echo "<script>location.href='login.php'</script>";
+        }
+    }
+       
 }
-else{
-    echo "<script> alert ('Username and Password is not matching!!')</script>";
-    echo "<script> location.href = 'login.php' </script>";
-}
-?> 
+?>
 
 
 
